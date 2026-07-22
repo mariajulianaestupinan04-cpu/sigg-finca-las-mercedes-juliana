e836398802ed57577d21edff87908d7cff9ea015
mariajulianaestupinan04-cpu <maria.julianaestupinan04@gmail.com>
2026-07-22 19:08:18 +0000

    feat:activar año dinamico en el pie de pagina

--- js/main.js
+++ js/main.js
@@ -33,3 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+document.addEventListener("DOMContentLoaded", () => {
+    const anio = document.getElementById("anio");
+    if (anio) anio.textContent = new Date().getFullYear();
+});
